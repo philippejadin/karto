cd483ff08fc2d209a3c8c5861b928e37784e12d5
philippejadin <philippe.jadin@gmail.com>
2016-04-01 14:53:28 +0200

    Modèle contact et migration contact

---
+++ database/migrations/2016_04_01_125241_create_contact_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateContactTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // ici ajouter les champs nécessaires pour la base de donnée, à réfléchir
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
