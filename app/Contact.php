68b4551c55af5b9825d352cb4ace8a37cf25c789
philippejadin <philippe.jadin@gmail.com>
2016-04-06 12:46:20 +0200

    supressions modèles inutiles

--- app/Contact.php
+++ app/Contact.php
@@ -6,5 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 
 class Contact extends Model
 {
-    //
+  protected $dates = ['deleted_at'];
+
+  protected $table    = 'contacts';
+
+  protected $fillable = [
+    'name',
+    'description',
+    'adress',
+    'postal_code',
+    'locality',
+    'country',
+    'phone',
+    'phone2',
+    'website',
+    'email',
+    'public',
+    'latitude',
+    'longitude',
+    'uuid'
+  ];
+
+
+  
 }
--- app/Http/Controllers/Admin/ContactsController.php
+++ app/Http/Controllers/Admin/ContactsController.php
@@ -15,103 +15,99 @@ use Illuminate\Http\Request;
 class ContactsController extends Controller {
 
 	/**
-	 * Display a listing of contacts
-	 *
-     * @param Request $request
-     *
-     * @return \Illuminate\View\View
-	 */
+	* Display a listing of contacts
+	*
+	* @param Request $request
+	*
+	* @return \Illuminate\View\View
+	*/
 	public function index(Request $request)
-    {
-        $contacts = Contacts::all();
+	{
+		$contacts = Contacts::all();
 
 		return view('admin.contacts.index', compact('contacts'));
 	}
 
 	/**
-	 * Show the form for creating a new contacts
-	 *
-     * @return \Illuminate\View\View
-	 */
+	* Show the form for creating a new contacts
+	*
+	* @return \Illuminate\View\View
+	*/
 	public function create()
 	{
-	    
-	    
-	    return view('admin.contacts.create');
+
+
+		return view('admin.contacts.create');
 	}
 
 	/**
-	 * Store a newly created contacts in storage.
-	 *
-     * @param CreateContactsRequest|Request $request
-	 */
+	* Store a newly created contacts in storage.
+	*
+	* @param CreateContactsRequest|Request $request
+	*/
 	public function store(CreateContactsRequest $request)
 	{
-	    
+
 		Contacts::create($request->all());
 
 		return redirect()->route('admin.contacts.index');
 	}
 
 	/**
-	 * Show the form for editing the specified contacts.
-	 *
-	 * @param  int  $id
-     * @return \Illuminate\View\View
-	 */
+	* Show the form for editing the specified contacts.
+	*
+	* @param  int  $id
+	* @return \Illuminate\View\View
+	*/
 	public function edit($id)
 	{
 		$contacts = Contacts::find($id);
-	    
-	    
+
+
 		return view('admin.contacts.edit', compact('contacts'));
 	}
 
 	/**
-	 * Update the specified contacts in storage.
-     * @param UpdateContactsRequest|Request $request
-     *
-	 * @param  int  $id
-	 */
+	* Update the specified contacts in storage.
+	* @param UpdateContactsRequest|Request $request
+	*
+	* @param  int  $id
+	*/
 	public function update($id, UpdateContactsRequest $request)
 	{
 		$contacts = Contacts::findOrFail($id);
-
-        
-
 		$contacts->update($request->all());
 
 		return redirect()->route('admin.contacts.index');
 	}
 
 	/**
-	 * Remove the specified contacts from storage.
-	 *
-	 * @param  int  $id
-	 */
+	* Remove the specified contacts from storage.
+	*
+	* @param  int  $id
+	*/
 	public function destroy($id)
 	{
 		Contacts::destroy($id);
-
 		return redirect()->route('admin.contacts.index');
 	}
 
-    /**
-     * Mass delete function from index page
-     * @param Request $request
-     *
-     * @return mixed
-     */
-    public function massDelete(Request $request)
-    {
-        if ($request->get('toDelete') != 'mass') {
-            $toDelete = json_decode($request->get('toDelete'));
-            Contacts::destroy($toDelete);
-        } else {
-            Contacts::whereNotNull('id')->delete();
-        }
+	/**
+	* Mass delete function from index page
+	* @param Request $request
+	*
+	* @return mixed
+	*/
+	public function massDelete(Request $request)
+	{
+		if ($request->get('toDelete') != 'mass') {
+			$toDelete = json_decode($request->get('toDelete'));
+			Contacts::destroy($toDelete);
+		} else {
+			Contacts::whereNotNull('id')->delete();
+		}
 
-        return redirect()->route('admin.contacts.index');
-    }
+		return redirect()->route('admin.contacts.index');
+	}
 
 }
