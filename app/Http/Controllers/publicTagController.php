dd0adc9aebdfb9d3b1cefed98a93bf121df30b1d
Lilotresor <lilotresor@gmail.com>
2016-04-22 11:35:13 +0200

    Recherche par km

---
+++ app/Http/Controllers/publicTagController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class publicTagController extends Controller
+{
+    public function afficherContactsParTag(){
+    	
+    }
+}
