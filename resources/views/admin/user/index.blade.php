3c739c1343aec91894f7773e54f7b924664ad10e
philippejadin <philippe.jadin@gmail.com>
2016-09-09 11:51:42 +0200

    USer manager and navigation item

--- resources/views/admin/user/index.blade.php
+++ resources/views/admin/user/index.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="container-fluid">
 
-        <h1>Liste des utilisateur</h1>
+        <h1>Liste des utilisateurs</h1>
 
         <table class="table special">
             <thead>
@@ -17,7 +17,8 @@
             <tbody>
                 @foreach ($users as $user)
                     <tr>
-                        <td>{!! $user->email !!}</td>
+                        <td>{{ $user->email }} @if ($user->isAdmin()) (<i class="fa fa-bolt" aria-hidden="true"></i>
+ Administrateur) @endif</td>
                         <td>
                             <a href="{{ route('admin.user.edit', [$user->id]) }}" class="btn btn-success"><i class="fa fa-pencil-square-o"></i></a>
                         </td>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,6 +87,17 @@
                             </ul>
                         </li>
 
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                Utilisateurs <span class="caret"></span>
+                            </a>
+
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{action('UserController@index')}}">Liste</a></li>
+                            </ul>
+                        </li>
+
+
 
 
 
