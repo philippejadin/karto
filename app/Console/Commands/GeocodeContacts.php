c036a2ec238dc65861e8c50732983110bfd3bf8f
philippejadin <philippe.jadin@gmail.com>
2017-03-27 13:52:49 +0200

    add force option to geocode

--- app/Console/Commands/GeocodeContacts.php
+++ app/Console/Commands/GeocodeContacts.php
@@ -6,65 +6,69 @@ use Illuminate\Console\Command;
 
 class GeocodeContacts extends Command
 {
-    /**
-    * The name and signature of the console command.
-    *
-    * @var string
-    */
-    protected $signature = 'karto:geocode {size=500} {--retry}';
+  /**
+  * The name and signature of the console command.
+  *
+  * @var string
+  */
+  protected $signature = 'karto:geocode {size=500} {--retry} {--force}';
 
-    /**
-    * The console command description.
-    *
-    * @var string
-    */
-    protected $description = 'Geocode les contacts pas encore géocodés';
+  /**
+  * The console command description.
+  *
+  * @var string
+  */
+  protected $description = 'Geocode les contacts pas encore géocodés';
 
-    /**
-    * Create a new command instance.
-    *
-    * @return void
-    */
-    public function __construct()
+  /**
+  * Create a new command instance.
+  *
+  * @return void
+  */
+  public function __construct()
+  {
+    parent::__construct();
+  }
+
+  /**
+  * Execute the console command.
+  *
+  * @return mixed
+  */
+  public function handle()
+  {
+    // trouver 5 contacts sans longitude ou latitude
+    // les geocoder et le sauver (cfr. le modèle contact)
+
+    if ($this->option('retry'))
     {
-        parent::__construct();
+      $contacts = \App\Contact::where('geocode_status', '<', 0)->take($this->argument('size'))->get();
+    }
+    elseif ($this->option('force'))
+    {
+      $contacts = \App\Contact::take($this->argument('size'))->get();
+    }
+    else
+    {
+      $contacts = \App\Contact::where('geocode_status', '=', 0)->take($this->argument('size'))->get();
     }
 
-    /**
-    * Execute the console command.
-    *
-    * @return mixed
-    */
-    public function handle()
+
+
+    foreach ($contacts as $contact)
     {
-        // trouver 5 contacts sans longitude ou latitude
-        // les geocoder et le sauver (cfr. le modèle contact)
-
-        if ($this->option('retry'))
-        {
-            $contacts = \App\Contact::where('geocode_status', '<', 0)->take($this->argument('size'))->get();
-        }
-        else
-        {
-            $contacts = \App\Contact::where('geocode_status', '=', 0)->take($this->argument('size'))->get();
-        }
-
-
-
-        foreach ($contacts as $contact)
-        {
-            if ($contact->geocode(['force' => true]))
-            {
-                $contact->forceSave();
-                $this->info('Contact ' . $contact->name . ' correctement geolocalise');
-            }
-            else {
-                $contact->save(['force' => true]);
-                $this->error('Contact ' . $contact->name . ' pas geolocalise, geocode status : ' . $contact->geocode_status . ' / Message : ' . $contact->geocode_message);
-            }
-            //sleep(1);
-        }
+      if ($contact->geocode(['force' => true]))
+      {
+        $contact->forceSave();
+        $this->info('Contact ' . $contact->name . ' correctement geolocalise');
+      }
+      else {
+        $contact->save(['force' => true]);
+        $this->error('Contact ' . $contact->name . ' pas geolocalise, geocode status : ' . $contact->geocode_status . ' / Message : ' . $contact->geocode_message);
+      }
+      //sleep(1);
     }
+  }
 
 
 }
